b786deef989be74b06edcd426dc381586727fbfd
Guilherme Yonamine <113915589+GuilhermeYonamine@users.noreply.github.com>
2023-04-25 10:03:04 -0300

    Add files via upload

---
+++ web-service/springbootapi/src/test/java/ps2/mack/springbootapi/SpringbootapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ps2.mack.springbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
