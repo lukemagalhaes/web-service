17b32af0aeb51dc5f5a50c47d8ce44cdfe14c1b9
GuilhermeYonamine <guile.yonamine@gmail.com>
2023-05-09 01:13:00 -0300

    Commit exceptions feitas

--- web-service/springbootapi/src/main/java/ps2/mack/springbootapi/controller/ContaBancariaController.java
+++ web-service/springbootapi/src/main/java/ps2/mack/springbootapi/controller/ContaBancariaController.java
@@ -3,7 +3,11 @@ package ps2.mack.springbootapi.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import jakarta.validation.Valid;
 import ps2.mack.springbootapi.contaBancaria.ContaBancaria;
 import ps2.mack.springbootapi.contaBancaria.ContaBancariaRepository;
@@ -18,43 +22,75 @@ public class ContaBancariaController {
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
-    public void saveContaBancaria(@RequestBody @Valid ContaBancariaRequestDTO data) {
-        ContaBancaria contaData = new ContaBancaria(data);
-        repository.save(contaData);
-        return;
+    public ResponseEntity<Void> saveContaBancaria(@RequestBody @Valid ContaBancariaRequestDTO data) {
+        try {
+            ContaBancaria contaData = new ContaBancaria(data);
+            repository.save(contaData);
+            return ResponseEntity.ok().build();
+        } catch (Exception ex) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
-    public List<ContaBancariaResponseDTO> getAll() {
-        List<ContaBancariaResponseDTO> contaList = repository.findAll().stream()
-                .map(ContaBancariaResponseDTO::new).toList();
-        return contaList;
+    public ResponseEntity<List<ContaBancariaResponseDTO>> getAll() {
+        // Nao contem erro notfound, retorna lista vazia
+        try {
+            List<ContaBancariaResponseDTO> contaList = repository.findAll().stream()
+                    .map(ContaBancariaResponseDTO::new).toList();
+            return ResponseEntity.ok(contaList);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
-    @PutMapping
-    public ContaBancaria updateContaBancaria(@RequestBody ContaBancaria data){
-        if(data.getId()>0)
-            return repository.save(data);
-        return null;
+    @PutMapping("/{id}")
+    public ResponseEntity<ContaBancaria> updateContaBancaria(@RequestBody ContaBancaria data) {
+        try {
+            if (data.getId() > 0) {
+                ContaBancaria updatedContaBancaria = repository.save(data);
+                return ResponseEntity.ok(updatedContaBancaria);
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteContaBancaria(@PathVariable Long id) {
-        repository.deleteById(id);
+    public ResponseEntity<Void> deleteContaBancaria(@PathVariable Long id) {
+        try {
+            repository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
-    
+
     @GetMapping("/{id}")
-    public ContaBancariaResponseDTO getContaBancariaById(@PathVariable Long id) {
-        ContaBancaria conta = repository.findById(id).orElseThrow();
-        return new ContaBancariaResponseDTO(conta);
+    public ResponseEntity<ContaBancariaResponseDTO> getContaBancariaById(@PathVariable Long id) {
+        try {
+            ContaBancaria conta = repository.findById(id)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
+            return ResponseEntity.ok(new ContaBancariaResponseDTO(conta));
+        } catch (Exception ex) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/buscar")
-    public List<ContaBancariaResponseDTO> buscarPorNome(@RequestParam String conta) {
-        return repository.findByNomeTitularContainingIgnoreCase(conta)
-            .stream()
-            .map(ContaBancariaResponseDTO::new)
-            .collect(Collectors.toList());
+    public ResponseEntity<List<ContaBancariaResponseDTO>> buscarPorNome(@RequestParam String conta) {
+        try {
+            List<ContaBancariaResponseDTO> contaList = repository.findByNomeTitularContainingIgnoreCase(conta)
+                    .stream()
+                    .map(ContaBancariaResponseDTO::new)
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok().body(contaList);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
-
+    // http://localhost:8080/contas/buscar?conta=...
 }
--- web-service/springbootapi/src/main/java/ps2/mack/springbootapi/controller/ProdutoController.java
+++ web-service/springbootapi/src/main/java/ps2/mack/springbootapi/controller/ProdutoController.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.validation.Valid;
 import ps2.mack.springbootapi.produto.Produto;
@@ -20,43 +23,75 @@ public class ProdutoController {
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
-    public void saveProduto(@RequestBody @Valid ProdutoRequestDTO data) {
-        Produto produtoData = new Produto(data);
-        repository.save(produtoData);
-        return;
+    public ResponseEntity<Void> saveProduto(@RequestBody @Valid ProdutoRequestDTO data) {
+        try {
+            Produto produtoData = new Produto(data);
+            repository.save(produtoData);
+            return ResponseEntity.ok().build();
+        } catch (Exception ex) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
-    public List<ProdutoResponseDTO> getAll(){
-        List<ProdutoResponseDTO> produtoList = repository.findAll().stream().map(ProdutoResponseDTO::new).toList();
-        return produtoList;
+    public ResponseEntity<List<ProdutoResponseDTO>> getAll() {
+        // Nao contem erro notfound, retorna lista vazia
+        try {
+            List<ProdutoResponseDTO> produtoList = repository.findAll().stream()
+                    .map(ProdutoResponseDTO::new).toList();
+            return ResponseEntity.ok(produtoList);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
-    @PutMapping
-    public Produto updateProduto(@RequestBody Produto data){
-        if(data.getId()>0)
-            return repository.save(data);
-        return null;
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> updateProduto(@RequestBody Produto data) {
+        try {
+            if (data.getId() > 0) {
+                Produto updatedProduto = repository.save(data);
+                return ResponseEntity.ok(updatedProduto);
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteProduto(@PathVariable Long id) {
-        repository.deleteById(id);
+    public ResponseEntity<Void> deleteProduto(@PathVariable Long id) {
+        try {
+            repository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
     
     @GetMapping("/{id}")
-    public ProdutoResponseDTO getProdutoById(@PathVariable Long id) {
-        Produto produto = repository.findById(id).orElseThrow();
-        return new ProdutoResponseDTO(produto);  
+    public ResponseEntity<ProdutoResponseDTO> getProdutoById(@PathVariable Long id) {
+        try {
+            Produto produto = repository.findById(id)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
+            return ResponseEntity.ok(new ProdutoResponseDTO(produto));
+        } catch (Exception ex) {
+            return ResponseEntity.notFound().build();
+        }
     }
     
     @GetMapping("/buscar")
-    public List<ProdutoResponseDTO> buscarPorNome(@RequestParam String produto) {
-        return repository.findByDescricaoContainingIgnoreCase(produto)
-            .stream()
-            .map(ProdutoResponseDTO::new)
-            .collect(Collectors.toList());
+    public ResponseEntity<List<ProdutoResponseDTO>> buscarPorNome(@RequestParam String produto) {
+        try {
+            List<ProdutoResponseDTO> produtoList = repository.findByDescricaoContainingIgnoreCase(produto)
+                    .stream()
+                    .map(ProdutoResponseDTO::new)
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok().body(produtoList);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
     // http://localhost:8080/produtos/buscar?produto=...
 }
