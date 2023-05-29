c6b46d9d189e1e5bcd5517288bb8cdc46d3be2aa
GuilhermeYonamine <guile.yonamine@gmail.com>
2023-05-29 15:18:22 -0300

    Parte 1 das configuracoes do swagger

---
+++ web-service/springbootapi/src/main/java/ps2/mack/springbootapi/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package ps2.mack.springbootapi.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+    @Value("${bezkoder.openapi.dev-url}")
+    private String devUrl;
+
+    @Value("${bezkoder.openapi.prod-url}")
+    private String prodUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        Server prodServer = new Server();
+        prodServer.setUrl(prodUrl);
+        prodServer.setDescription("Server URL in Production environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("bezkoder@gmail.com");
+        contact.setName("BezKoder");
+        contact.setUrl("https://www.bezkoder.com");
+
+        Info info = new Info()
+                .title("Documentação da API do Projeto")
+                .version("1.0")
+                .description("This API exposes endpoints to manage tutorials.");
+        return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+    }
+}
